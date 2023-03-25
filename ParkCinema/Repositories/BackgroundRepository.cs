580a62b3fc39f80abba616902e21ef304966351f
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2023-03-26 01:23:04 +0400

    Cover photo is changed

--- ParkCinema/Repositories/BackgroundRepository.cs
+++ ParkCinema/Repositories/BackgroundRepository.cs
@@ -14,8 +14,8 @@ namespace ParkCinema.Repositories
         {
             return new List<BackgroundImage>
             {
-                new BackgroundImage{Id=1,MovieName="Kutsal Damacana 4",MovieDate="26 January", ImagePath="/images/kutsal.jpg"},
-                new BackgroundImage{Id=2,MovieName="Prestij Meselesi",MovieDate="2 February",ImagePath="/images/prestij.jpg"},
+                new BackgroundImage{Id=1,MovieName="Prestij Meselesi",MovieDate="26 January",ImagePath="/images/prestij.jpg"},
+                new BackgroundImage{Id=2,MovieName="Kutsal Damacana 4",MovieDate="2 February", ImagePath="/images/kutsal.jpg"},
                 new BackgroundImage{Id=3,MovieName="Evdə qalmış",MovieDate="9 February",ImagePath="/images/evde.jpg"},
                 new BackgroundImage{Id=4,MovieName="Ant-Man and the Wasp: Quantumania",MovieDate="16 February",ImagePath="/images/ant.jpg"},
                 new BackgroundImage{Id=5,MovieName="Shazam! Fury of the Gods",MovieDate="16 March",ImagePath="/images/shazam.jpg"},
